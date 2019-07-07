5f1407e704013a23ba0c26c444c3be4aad227a7c
Zachary Liou <zachliou@gmail.com>
2019-07-06 18:02:39 -0700

    Init

---
+++ app/src/main/java/com/zachliou/animations_lab/WatchActivity.java
@@ -0,0 +1,13 @@
+package com.zachliou.animations_lab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WatchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_watch);
+    }
+}
